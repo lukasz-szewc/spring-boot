0fbaafe5a7405b6de85e1b16d4e8734c9b77a62d
Lukasz Szewc <lukasz-szewc@users.noreply.github.com>
2018-08-08 20:19:21 +0200

    initializers + listeners

--- spring-boot-samples/spring-boot-sample-xml/src/main/java/sample/xml/AppCtxStart.java
+++ spring-boot-samples/spring-boot-sample-xml/src/main/java/sample/xml/AppCtxStart.java
@@ -1,19 +1,48 @@
 package sample.xml;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+import org.omg.PortableInterceptor.LOCATION_FORWARD;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 import sample.xml.service.HelloWorldService;
 
 public class AppCtxStart {
 
+    public static final Logger LOGGER = LoggerFactory.getLogger(AppCtxStart.class);
+
     public static void main(String[] args) {
         Object[] sources = {HelloWorldService.class};
         SpringApplication springApplication = new SpringApplication(sources);
+        springApplication.addListeners(new ApplicationEventApplicationListener());
+        springApplication.addInitializers(new ConfigurableApplicationContextApplicationContextInitializer());
+        springApplication.getListeners().forEach(applicationListener -> LOGGER.info("application listener {}", applicationListener));
+        springApplication.getSources().forEach(o -> LOGGER.info("Source {}", o));
+        LOGGER.info(springApplication.getMainApplicationClass().getCanonicalName());
+        springApplication.setBannerMode(Banner.Mode.OFF);
         ConfigurableApplicationContext context = springApplication.run(args);
-
-        HelloWorldService bean = context.getBean(HelloWorldService.class);
-        System.out.println(bean.getAnotherProperty());
+        ConfigurableEnvironment environment = context.getEnvironment();
 
         context.stop();
+    }
+
+    private static class ApplicationEventApplicationListener implements ApplicationListener<ApplicationEvent> {
+        @Override
+        public void onApplicationEvent(ApplicationEvent event) {
+            LOGGER.info("Event happened: {}" , event.toString());
+        }
+    }
+
+    private static class ConfigurableApplicationContextApplicationContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            LOGGER.info("Instance of app context {}" , applicationContext);
+        }
     }
 }
